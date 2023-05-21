b27a2202a7f303042f9f3062bffb79a9e2997000
BoodyElemary <abdelrahman_elemary99@outlook.com>
2023-05-21 21:39:09 +0300

    finishing labs

---
+++ student/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from .models import Student
+from .forms import StudentAddForm, StudentUpdateForm
+from django.views import View
+# Create your views here.
+
+
+def index(req):
+
+    students = Student.objects.all()
+    return render(req, "student/index.html", {"students": students})
+
+
+def add(req):
+
+    if req.method == 'POST':
+        form = StudentAddForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/student")
+    else:
+        form = StudentAddForm()
+
+    return render(req, 'student/add-student.html', {'form': form})
+
+
+def update(req, id):
+
+    student = Student.objects.get(id=id)
+    if req.method == 'POST':
+        form = StudentUpdateForm(req.POST, instance=student)
+        if form.is_valid():
+            # print(form)
+            form.save()
+            return HttpResponseRedirect("/student")
+    else:
+        form = StudentUpdateForm(instance=student)
+
+    return render(req, 'student/update-student.html', {'form': form})
+
+
+def delete(req, id):
+    return HttpResponseRedirect("/student")
